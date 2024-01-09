a3c94c6cf4ab4a7b0ba78eb7649ba0f8fd7565af
ozodbekqurbanbayev <ozodbekqurbanbayev536@gmail.com>
2024-01-09 21:05:52 +0300

    add js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+const elDarktext = document.querySelector(".dark-text");
+elDarktext.addEventListener("click" , function () {
+  document.body.classList.remove("dark-mode");
+});
+
+
 const elDarkimg = document.querySelector(".dark-img");
 elDarkimg.addEventListener("click" , function () {
   document.body.classList.add("dark-mode");
 });
 
-const elDarktext = document.querySelector(".dark-text");
-elDarktext.addEventListener("click" , function () {
-  document.body.classList.remove("dark-mode");
-});
